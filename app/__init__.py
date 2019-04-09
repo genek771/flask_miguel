d7f15160bc454c170aef15c8be479321a6d5fd2f
genek771 <genek771@yandex.ru>
2019-04-09 17:16:46 +0300

    Часть 4: База данных

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from config import Config
 
+from flask_sqlalchemy import SQLAlchemy 
+from flask_migrate import Migrate
+
 app = Flask(__name__)
 app.config.from_object(Config)
-# app.config['ENV'] = 'development'
-# app.config['DEBUG'] = True
-# app.config['TESTING'] = True
 
-from app import routes
+db = SQLAlchemy(app) 
+migrate = Migrate(app, db) 
+ 
+from app import routes, models 
