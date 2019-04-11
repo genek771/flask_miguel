2ee74e337f376e1c3ac49f58bd214af100f8bd54
genek771 <genek771@yandex.ru>
2019-04-11 10:24:33 +0300

    Часть 6: Страница профиля и аватары

--- app/forms.py
+++ app/forms.py
@@ -5,6 +5,8 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 
 from app.models import User
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired, Length 
 
 
 
@@ -57,3 +59,8 @@ class RegistrationForm(FlaskForm):
         if user is not None:
 
             raise ValidationError('Please use a different email address.')
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
